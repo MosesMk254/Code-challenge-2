a18d17871ae343ef09246d74da8f14ca93c4705c
Moses Mutisya <mutisyamoses722@gmail.com>
2024-04-01 13:00:27 +0300

    changing the cases of the characters of a string

--- case-changer.js
+++ case-changer.js
@@ -1,6 +1,10 @@
-function caseChanger(string) {
-  let changer = string.split(" ");
+// A javascript function that takes in a string and changes the cases of the string and returns the modified string
 
+// the function that takes in the string as its parameter and checks it through the conditions and modifies it according to the conditions.
+function caseChanger(string) {
+  let changer = string.split(" "); // assigning a variable that splits the string into individual character
+
+  // a loop that iterates through the string to check if it has lower case or upper case
   for (let i = 0; i < changer.length; i++) {
     let changed = changer[i];
 
@@ -8,11 +12,9 @@ function caseChanger(string) {
       changer[i] = changed.toLowerCase();
     } else if (changed === changed.toLowerCase()) {
       changer[i] = changed.toUpperCase();
-    } else {
-      ("Not a valid answer");
     }
   }
-  let newString = changer.join(" ");
-  return newString;
+  let newString = changer.join(" "); //a variable that joins the characters of the string to one string
+  return newString; // returning the modified string depending on the condition it matches
 }
-console.log(caseChanger("My Name Is Moses"));
+console.log(caseChanger("my name is moses"));
